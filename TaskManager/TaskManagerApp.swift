d47f58d98922b77a19c25b7f3f8317e004f73a87
audblog <2010.audreyc@gmail.com>
2024-06-12 11:47:12 -0500

    Initial Commit

---
+++ TaskManager/TaskManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaskManagerApp.swift
+//  TaskManager
+//
+//  Created by Scholar on 6/12/24.
+//
+
+import SwiftUI
+
+@main
+struct TaskManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
